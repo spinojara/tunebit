3781f87bb52cd025134be5be23d69af2f938fcd1
Isak Ellmer <isak01@gmail.com>
2024-07-22 14:14:16 +0200

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='tunebit'
+        version='0.0a0',
+        scripts=['bin/tunebit'],
+        packages=['tunebit'],
+        author='Isak Ellmer',
+        author_email='isak01@gmail.com',
+        description='A search parameter tuner for bitbit.',
+        url='https://github.com/spinojara/tunebit',
+)
